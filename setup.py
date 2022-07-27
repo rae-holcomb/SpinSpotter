f5825085a47b59c25de4ab593d7c2ebf92119242
rae-holcomb <raeholcomb15@gmail.com>
2022-07-27 16:02:53 -0700

    rtd - add requirements, change setup.py to find_packages()

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='spinspotter',
@@ -8,7 +8,7 @@ setup(
     author='Rae Holcomb',
     author_email='raeholcomb15@gmail.com',
     license='BSD 3-clause',
-    packages=['SpinSpotter'],
+    packages=find_packages(),
     install_requires=['matplotlib',
                       'numpy',  
                       'pandas',
